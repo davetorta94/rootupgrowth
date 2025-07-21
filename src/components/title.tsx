c553002e9d4c18ad3e5d81b8a4d0e7e07a033c70
David Torta <davidtortaolah@gmail.com>
2025-07-21 06:35:42 +0200

    title visibility fixed

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -4,7 +4,7 @@ export const Title = () => {
   return (
     <>
     
-      <div className='hidden md:block animate-fade-up animate-ease-in-out animate-delay-[1500ms]'>
+      <div className='animate-fade-up animate-ease-in-out animate-delay-[1500ms]'>
             <p className='text-2xl text-[#5a22f5]'>Welcome to</p>
             <h1 className='text-9xl text-[#5a22f5]'>ROOT UP <br /> GROWTH</h1>
       </div>
