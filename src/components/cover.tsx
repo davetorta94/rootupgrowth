68b48dcc6caa656aa9d575f839872658eb258309
David Torta <davidtortaolah@gmail.com>
2025-07-21 06:43:18 +0200

    margin fixed in cover

--- src/components/cover.tsx
+++ src/components/cover.tsx
@@ -5,7 +5,7 @@ import { TypingAnimation } from "@/components/magicui/typing-animation";
 export const Cover = () => {
   return (
     <>
-          <div className="flex justify-center">
+          <div className="flex justify-center mt-68 h-10 md:mt-32">
                 <h2 className='text-8xl text-[#5a22f5]'>
                 <TypingAnimation startOnView duration={100}>MI PEQUEÑO JARDIN</TypingAnimation>
                 </h2>
