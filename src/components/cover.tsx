bafda6ebda47febfb91ce040604093305af5c00b
David Torta <davidtortaolah@gmail.com>
2025-07-26 07:08:20 +0200

    padding fixed for narrow width

--- src/components/cover.tsx
+++ src/components/cover.tsx
@@ -5,7 +5,7 @@ import { TypingAnimation } from "@/components/magicui/typing-animation";
 export const Cover = () => {
   return (
     <>
-          <div className="flex justify-center mt-68 h-10 md:mt-32">
+          <div className="flex justify-center h-160 pt-[1000px] sm:mt-68 sm:h-10 md:mt-32">
                 <h2 className='text-8xl text-[#5a22f5]'>
                 <TypingAnimation startOnView duration={100}>MI PEQUEÑO JARDIN</TypingAnimation>
                 </h2>
