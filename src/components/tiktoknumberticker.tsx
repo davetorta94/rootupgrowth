07fdeec0149dc7bd212992ba4cf17ac78794d158
David Torta <davidtortaolah@gmail.com>
2025-10-24 20:58:30 +0200

    butterfly added test

--- src/components/tiktoknumberticker.tsx
+++ src/components/tiktoknumberticker.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image"
 import { Highlighter } from "./magicui/highlighter"
 import { NumberTicker } from "./magicui/number-ticker"
 import { TypingAnimation } from "./magicui/typing-animation"
+import  mariposa  from "../../public/mariposa.png"
 
 
 export function TiktokNumberTicker() {
@@ -16,9 +18,11 @@ export function TiktokNumberTicker() {
 
      <TypingAnimation className="text-amber-50 text-center text-2xl">FOLLOWERS EN MENOS DE UN AÑO</TypingAnimation>
 
-    <div className="animate-fade-up animate-ease-in-out animate-delay-[1500ms] pt-2 text-center items-middle text-2xl">
+    <div className="animate-fade-up animate-delay-[1500ms] pt-2 text-center items-middle text-2xl">
         <Highlighter>Y CRECIENDO...</Highlighter>
     </div>
+
+    <Image src={mariposa} width={400} height={400} alt="background flower" className="animate-rotate-y animate-delay-[1500ms] hidden md:flex" />
 
     </div>
     </>
