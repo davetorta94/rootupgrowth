83c49834b13e3ecc4f0b32c1a05d8f7dec69769c
David Torta <davidtortaolah@gmail.com>
2025-12-03 09:54:40 +0100

    fix 2

--- src/app/form/FormPage.tsx
+++ src/app/form/FormPage.tsx
@@ -78,7 +78,7 @@ export default function FormPage( ) {
           name={name}
           email={email}
           message={message}
-          plan={plan}
+          plan={planame}
           onInputChange={onInputChange}
           onSubmit={handleSubmit}
         />
